2f7a0a70c0edf127be9df1893a3f32ef0037b54d
priya <priyajaw>
2022-10-24 22:04:48 +0530

    started with signin form

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,46 @@
+const User=require('../models/user');
+
+
 module.exports.profile=function(req,res){
     return res.render('user_profile',{
         title:" profile page"
     })
 }
+module.exports.signUp=function(req,res){
+    return res.render('signUp',{
+        title:"sign Up"
+    })
+}
+module.exports.signIn=function(req,res){
+    return res.render('signIn',{
+        title:"sign In"
+    })
+}
+// get up sign up data
+module.exports.create=function(req,res){
+ if(req.body.password!=req.body.confirm_password){
+    return res.redirect('back');
+ } 
+ User.findOne({email:req.body.email},function(err,user){
+    if(err){
+        console.log('error finding user');
+        return}
+    if(!user){
+        User.create(req.body,function(err,user){
+            if(err){
+                console.log('error in creating user'); return;
+            }
+            return res.redirect('/users/sign-in');
+
+        })
+
+    }else{
+        return res.redirect('back')
+
+    }
+    
+ }); 
+}
+// module.exports.createSession=funcion(req,res){
+
+// }
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express=require('express');
+//adding cookie parser
+
+const cookieParser=require('cookie-parser');
+
 const app=express();
   const port=8000;
+const db=require('./config/mongoose');
 
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
 const expressLayouts=require('express-ejs-layouts');
 
 app.use(express.static('./assests'));
