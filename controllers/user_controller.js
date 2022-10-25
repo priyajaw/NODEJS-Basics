4e87bfaa838df59a73dbbb3289dbec821416a1cf
priya <priyajaw>
2022-10-25 14:23:13 +0530

    done with manual  auth

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,9 +2,23 @@ const User=require('../models/user');
 
 
 module.exports.profile=function(req,res){
-    return res.render('user_profile',{
-        title:" profile page"
-    })
+   if(req.cookies.user_id){
+    User.findById(req.cookies.user_id,function(err,user){
+        if(user){
+            return res.render('user_profile',{
+                title:"user profile",
+                user:user
+            })
+        }
+         
+            return res.redirect('/users/sign-in')
+    });
+
+ 
+}
+else{
+    return res.redirect('/users/sign-in')
+}
 }
 module.exports.signUp=function(req,res){
     return res.render('signUp',{
@@ -41,6 +55,37 @@ module.exports.create=function(req,res){
     
  }); 
 }
-// module.exports.createSession=funcion(req,res){
+module.exports.createSession=function(req,res){
+//find the user
 
-// }
+User.findOne({email:req.body.email},function(err,user){
+    if(err){
+        console.log('error in finding user');
+        return;
+
+//handle user found
+    }
+    if(user){
+        
+//handle password which don't match
+
+        if(user.password!=req.body.password){
+            return res.redirect('back');
+        }
+
+
+
+
+//handle session creation
+res.cookie('user_id',user.id);
+return res.redirect('/users/profile');
+
+    }
+    else{
+        
+//handle user not found
+return res.redirect('back');
+    }
+})
+
+}
--- routes/user.js
+++ routes/user.js
@@ -10,5 +10,9 @@ router.get('/sign-in',userController.signIn);
 
 
 router.post('/create',userController.create);
-
+router.post('/create-session',userController.createSession);
+router.get('/logout', function (req, res) {
+    res.clearCookie('connect.sid');
+    res.redirect('/users/sign-up');
+  });
 module.exports=router;
